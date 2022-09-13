f3387b7d2ae8cc0f5c21be21a4c13a62e28f4730
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-09-13 11:52:00 -0300

    feat: finish structure in landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import "./styles/main.css";
+import { MagnifyingGlassPlus } from "phosphor-react"
 import logoImg from "./assets/logo-nlw-esports.svg";
 
 // JSX = JavaSript + XML (HTML)
@@ -8,9 +9,9 @@ function App() {
       <img src={logoImg} alt="" />
 
       <h1 className="text-6xl text-white font-black mt-20 ">
-        Seu
+        Seu&nbsp;
         <span className="text-transparent bg-nlw-gradient bg-clip-text">
-          duo
+          duo&nbsp;
         </span>
         está aqui.
       </h1>
@@ -64,6 +65,24 @@ function App() {
             <span className="text-zinc-300 text-sm block">4 anúncios</span>
           </div>
         </a>
+      </div>
+
+      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#212634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   );
