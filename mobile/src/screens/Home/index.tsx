f484011e75345144e287db03c5eeb9d5fd6e847d
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-09-13 13:42:53 -0300

    feat: create Home screen

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,26 @@
+import { View, Image, FlatList } from "react-native";
+import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { GAMES } from "../../utils/games";
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+        horizontal
+      />
+    </View>
+  );
+}
