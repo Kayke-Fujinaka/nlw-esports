f7d46d2427f94f6f3744b4dc655e47af804914d5
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-09-13 11:30:58 -0300

    config: postcss and tailwind

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx", "./index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        "nlw-gradient":
+          "linear-gradient(90deg, #9572FC 20%, #43E7AD 65%, #E1D55D 20%)",
+        "game-gradient": "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
+      },
+      colors: {},
+    },
+  },
+  plugins: [],
+};
