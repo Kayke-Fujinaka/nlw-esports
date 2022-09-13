deb492e96f2c1137a24b85a104cd6d5d94018402
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-09-13 13:42:10 -0300

    feat: create Background component

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,17 @@
+import { ImageBackground } from 'react-native';
+import backgroundImg from "../../assets/background-galaxy.png"
+import { styles } from './styles';
+
+interface Props {
+    children: React.ReactNode
+}
+
+export function Background({children}: Props) {
+  return (
+    <ImageBackground source={backgroundImg} defaultSource={backgroundImg} style={styles.container}>
+        {children}
+    </ImageBackground>
+  );
+}
+
+// O defaultSource ele memoriza a imagem padrão carregando mais rápido a imagem na aplicação
