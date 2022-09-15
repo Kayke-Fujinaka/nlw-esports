4cac155adec612b32bbbe24c511d4cd1d1070579
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-09-14 23:42:10 -0300

    feat: finish routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,35 +1,103 @@
 import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import { convertHourStringToMinutes } from "./utils/convert-hour-string-to-minutes";
+import { convertMinutesToHourString } from "./utils/convert-minute-to-hour-string";
 
-const app = express()
+const app = express();
+app.use(express.json());
+app.use(cors())
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 // Listagem de games com contagem de anúncios
-app.get('/games', (resquest, response) => {
-    return response.json([])
-})
-
-// Criação de novo anúncio
-app.post('/ads', (request, response) => {
-    return response.status(201).json([])
-})
+app.get("/games", async (resquest, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+  return response.json(games);
+});
 
 // Listagem de anúncios por game (game específico)
-app.get('/games/:id/ads', (request, response) => {
-    return response.json([
-        {id: 1, name: "Anúncio 1"},
-        {id: 2, name: "Anúncio 2"},
-        {id: 3, name: "Anúncio 3"},
-        {id: 4, name: "Anúncio 4"},
-        {id: 5, name: "Anúncio 5"},
-        {id: 6, name: "Anúncio 6"},
-    ])
-})
+app.get("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return response.json(
+    ads.map((ad) => {
+      return {
+        ...ad,
+        weekDays: ad.weekDays.split(","),
+        hourStart: convertMinutesToHourString(ad.hourStart),
+        hourEnd: convertMinutesToHourString(ad.hourEnd),
+      };
+    })
+  );
+});
 
 // Buscar o discord pelo ID do anúncio
-app.get('/ads/:id/discord', (request, response) => {
-    return response.json([])
-})
+app.get("/ads/:id/discord", async (request, response) => {
+  const adId = request.params.id;
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
 
-app.listen(3333)
+  return response.json({
+    discord: ad.discord,
+  });
+});
+
+// Criação de novo anúncio
+app.post("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
+  const body: any = request.body;
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(","),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  });
+
+  return response.status(201).json(ad);
+});
+
+app.listen(3333);
 
 /* 
 HTTP Method
---
+++ server/src/utils/convert-hour-string-to-minutes.ts
@@ -0,0 +1,8 @@
+// 1080 -> ["18", "00"] -> [18, 00]
+
+export function convertHourStringToMinutes(hourString: string) {
+  const [hours, minutes] = hourString.split(":").map(Number);
+
+  const minutesAmount = hours * 60 + minutes;
+  return minutesAmount;
+}
